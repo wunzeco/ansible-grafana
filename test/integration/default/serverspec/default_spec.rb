ec1891e4f9267753eab935a5b4d2b9546764c729
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-07-08 11:27:47 +0100

    [OI] add support for grafana plugins

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -10,10 +10,31 @@ describe file('/etc/grafana/grafana.ini') do
   it { should be_mode 640 }
 end
 
+describe file('/etc/grafana/ldap.toml') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_mode 640 }
+end
+
 describe file('/etc/default/grafana-server') do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_mode 644 }
+end
+
+%w( 
+  grafana-piechart-panel
+  grafana-worldmap-panel
+).each do |plugin|
+  describe file("/var/lib/grafana/plugins/#{plugin}") do
+    it { should be_directory }
+    it { should be_owned_by 'root' }
+    it { should be_mode 755 }
+  end
+end
+
+describe file('/var/lib/grafana/plugins/grafana-example-app') do
+  it { should_not exist }
 end
 
 describe service('grafana') do
